b685d8ff7708de58950a0db9545d73dfe1dd7a89
Daniela3012 <danielapaola301299@gmail.com>
2026-02-15 08:59:59 -0500

    nuevo nivel

---
+++ src/Level4/InsertWords.java
@@ -0,0 +1,20 @@
+package Level4;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class InsertWords {
+    public static void main(String[] args) {
+        Queue<String> words = new LinkedList<>();
+        words.offer("xd");
+        words.offer("xxx");
+        words.offer("ddd");
+        words.offer("###");
+
+        System.out.println(words.peek());
+        System.out.println();
+        for (String word: words) {
+            System.out.print(word+"  ");
+        }
+    }
+}
